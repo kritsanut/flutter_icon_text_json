425762ba9952a0b19f7730fee6f23e22f18a2924
Krisanut T <kritsanut@users.noreply.github.com>
2021-01-27 12:00:09 +0700

    Create sample.dart

---
+++ sample.dart
@@ -0,0 +1,7 @@
+backgroundColor: Color(int.parse(cgIncident.priority_button_color),),
+
+
+Icon(IconData(int.parse("0x"+menuModel.icon_code),
+                  fontFamily:'Material Design Icons',
+                  fontPackage:'material_design_icons_flutter'),
+                  color: Colors.blueGrey, size: 45, ),
